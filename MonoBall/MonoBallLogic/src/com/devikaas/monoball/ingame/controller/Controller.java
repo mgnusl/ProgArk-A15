a212f2619b1e72df0e6c5ef3c3fdc6c32999de95
bvx89 <bvx89.at.cs@gmail.com>
2014-03-18 14:52:45 +0100

    Added controllers for touch and keyboard

---
+++ MonoBall/MonoBallLogic/src/com/devikaas/monoball/ingame/controller/Controller.java
@@ -0,0 +1,9 @@
+package com.devikaas.monoball.ingame.controller;
+
+import com.devikaas.monoball.ingame.model.Steppable;
+
+/**
+ * Created by bvx89 on 18/03/14.
+ */
+public interface Controller extends Steppable {
+}
