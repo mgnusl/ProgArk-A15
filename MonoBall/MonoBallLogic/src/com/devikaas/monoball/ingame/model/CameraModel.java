beb06b909e3185e306a9bac829e439a0893a864a
Oddwarg <mogfen@yahoo.com>
2014-03-22 02:20:42 +0100

    Grand Merge

--- MonoBall/MonoBallLogic/src/com/devikaas/monoball/ingame/model/CameraModel.java
+++ MonoBall/MonoBallLogic/src/com/devikaas/monoball/ingame/model/CameraModel.java
@@ -1,30 +1,54 @@
 package com.devikaas.monoball.ingame.model;
 
+import static owg.engine.Engine.scene;
 import owg.engine.graphics.ColorF;
 import owg.engine.graphics.ColorF.ColorFMutable;
 import owg.engine.util.V3F;
 /**The in-game camera. Defines the visible area of the game world.*/
 public class CameraModel implements Steppable {
 	private V3F location;
-	private V3F speed;
+	private float speed;
 	private float viewWidth, viewHeight;
 	
 	private ColorFMutable clearColor;
+    private final BallGameModel ballGameModel;
+
+    private static final float MARGIN = 200;
 	
 	public CameraModel(V3F location, float width, float height) {
 		this.location = location;
 		this.viewWidth = width;
 		this.viewHeight = height;
-		speed = new V3F();
+		speed = 0;
 		clearColor = ColorF.LTGRAY.getMutableCopy();
 	}
 	
 	@Override
 	public void step() {
-		location.add(speed);
-		//float screenAspect = (float)scene().getWidth()/scene().getHeight();
-        //viewHeight = 320;
-        //viewWidth = viewHeight*screenAspect;
+        float screenAspect = (float)scene().getWidth()/scene().getHeight();
+        viewHeight = 320;
+        viewWidth = viewHeight*screenAspect;
+
+        location.add(0, speed, 0);
+
+        // Check location of ball
+        float ballY = ballGameModel.getBall().getLocation().y();
+
+        if (ballGameModel.isReversed()) {
+            float totalBall = ballY - MARGIN;
+            float totalCam = location.y();
+            if (totalBall <= totalCam) {
+                location.add(0, totalBall - totalCam, 0);
+            }
+
+        } else {
+            float totalBall = ballY + MARGIN;
+            float totalCam = location.y()+viewHeight;
+            if (totalBall >= totalCam) {
+                location.add(0, totalBall - totalCam, 0);
+            }
+        }
+
 	}	
 	
 	public V3F getLocation() {
@@ -44,6 +68,8 @@ public class CameraModel implements Steppable {
 	}
 
 	public void setVerticalSpeed(float ySpeed) {
-		speed.y(ySpeed);
+		this.speed = ySpeed;
 	}
+
+    public void reverse() {speed *= -1;}
 }
---
+++ MonoBall/MonoBallLogic/src/com/devikaas/monoball/ingame/model/map/blocks/DeathBlock.java
@@ -0,0 +1,48 @@
+package com.devikaas.monoball.ingame.model.map.blocks;
+
+import com.devikaas.monoball.ingame.model.BallModel;
+import com.devikaas.monoball.ingame.model.map.Row;
+import com.devikaas.monoball.ingame.model.map.SolidLine;
+import com.devikaas.monoball.ingame.model.map.Collidable;
+/**
+ * Created by oknak_000 on 3/18/14.
+ */
+public class DeathBlock extends BasicBlock{
+	public DeathBlock(Row row, float xOffset, float width){
+		super(row, xOffset, width);
+	}
+
+	@Override
+	public String getSprite(){
+		return "deathBlock";
+	}
+
+	@Override
+	public void evaluateSurface(Collidable subject) {
+		BallModel ball = null;
+
+		for(SolidLine l : lines){
+			if(l.evaluateLine(subject) && subject instanceof BallModel){
+				ball = (BallModel)subject;
+			}
+		}
+
+		if(ball != null)
+			ball.kill();
+	}
+
+	@Override
+	public void evaluateEndpoints(Collidable subject) {
+		BallModel ball = null;
+		
+		for(SolidLine l : lines){
+			if(l.evaluateEndpoints(subject) && subject instanceof BallModel){
+				ball = (BallModel)subject;
+			}
+		}
+
+		if(ball != null)
+			ball.kill();
+	}
+
+}
