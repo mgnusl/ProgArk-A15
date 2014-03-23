eda0e05452ebb631a027b3f526ddd9186bcdcad3
bvx89 <bvx89.at.cs@gmail.com>
2014-03-23 18:28:43 +0100

    BlockFactory returns block object

--- MonoBall/MonoBallLogic/src/com/devikaas/monoball/ingame/model/map/BlockFactory.java
+++ MonoBall/MonoBallLogic/src/com/devikaas/monoball/ingame/model/map/BlockFactory.java
@@ -10,24 +10,22 @@ public class BlockFactory {
     public static final int BLOCKS_PER_LINE = 16;
 
 
-    public static void createBlock(Row r, int offset, char type) {
+    public static Block createBlock(Row r, int offset, char type) {
     	float blockWidth = (float)MapModel.MAP_WIDTH/BLOCKS_PER_LINE;
 
         switch(type){
             case BasicBlock.TYPE:
-                new BasicBlock(r, offset*blockWidth, blockWidth);
-                break;
+                return new BasicBlock(r, offset*blockWidth, blockWidth);
             case DeathBlock.TYPE:
-                new DeathBlock(r, offset*blockWidth, blockWidth);
-                break;
+                return new DeathBlock(r, offset*blockWidth, blockWidth);
             case FakeBlock.TYPE:
-                new FakeBlock(r, offset*blockWidth, blockWidth);
-                break;
+                return new FakeBlock(r, offset*blockWidth, blockWidth);
             case SpriteSwapBlock.TYPE:
-                new SpriteSwapBlock(r, offset*blockWidth, blockWidth);
-                break;
+                return new SpriteSwapBlock(r, offset*blockWidth, blockWidth);
             case StickyBlock.TYPE:
-                new StickyBlock(r, offset*blockWidth, blockWidth);
+                return new StickyBlock(r, offset*blockWidth, blockWidth);
+            default:
+                return null;
 		}
     }
 }
