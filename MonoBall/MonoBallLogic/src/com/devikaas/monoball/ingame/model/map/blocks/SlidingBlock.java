4f9c3bb0e7e81f3b389f3437f30bd2b7e52884de
bvx89 <bvx89.at.cs@gmail.com>
2014-03-23 18:31:12 +0100

    Skeleton for SlidingBlock

--- MonoBall/MonoBallLogic/src/com/devikaas/monoball/ingame/model/map/blocks/SlidingBlock.java
+++ MonoBall/MonoBallLogic/src/com/devikaas/monoball/ingame/model/map/blocks/SlidingBlock.java
@@ -1,7 +1,33 @@
 package com.devikaas.monoball.ingame.model.map.blocks;
 
-/**
- * Created by bvx89 on 23/03/14.
- */
-public class SlidingBlock {
+import com.devikaas.monoball.ingame.model.Steppable;
+import com.devikaas.monoball.ingame.model.map.Row;
+import owg.engine.util.Kryo;
+
+
+public class SlidingBlock extends BasicBlock implements Steppable {
+    public static final char TYPE = 'f';
+    private static final float SPEED = 20;
+
+    private int length = 0;
+    private boolean directionRight = false;
+
+    @Kryo
+    private SlidingBlock() {
+        super();
+    }
+
+    public SlidingBlock(Row row, float xOffset, float width){
+        super(row, xOffset, width);
+    }
+
+    @Override
+    public void step() {
+
+
+    }
+
+    public void setTrackLength(int length) {
+        this.length = length;
+    }
 }
