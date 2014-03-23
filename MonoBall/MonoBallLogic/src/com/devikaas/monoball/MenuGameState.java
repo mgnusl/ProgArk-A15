a30a767749789968f4005c85b7b52f9dfc5dd0d7
OleKN <ok.nakken@gmail.com>
2014-03-23 17:01:58 +0100

    Replaced placeholder files in menu, and added BG

--- MonoBall/MonoBallLogic/src/com/devikaas/monoball/MenuGameState.java
+++ MonoBall/MonoBallLogic/src/com/devikaas/monoball/MenuGameState.java
@@ -9,6 +9,7 @@ import owg.engine.Engine;
 import owg.engine.GameState;
 import owg.engine.graphics.ColorF;
 import owg.engine.graphics.MatrixStack;
+import owg.engine.graphics.Sprite2D;
 import owg.engine.util.Compass;
 import owg.engine.util.V3F;
 
@@ -26,14 +27,14 @@ public class MenuGameState implements GameState {
 
 	// Used to dynamically rotate logo.
 	private float logoRotation = 0.0f;
-	private float logoRotationSpeed = 0.0031f;
+	private float logoRotationSpeed = 0.0021f;
 	private float logoMaxAngle = 0.1f;
 
 	// Used to dynamically scale logo. logoMinScale will be set according to logoMaxScale.
 	// logoMaxScale will be changed to accomodate actual logoScale
 	private float logoMinScale;
 	private float logoMaxScale = 0.12f;
-	private float logoScaleSpeed = 0.0015f;
+	private float logoScaleSpeed = 0.0011f;
 
 	//Stores position and size of buttons, to allow easy interaction
 	private V3F playPos;
@@ -62,7 +63,7 @@ public class MenuGameState implements GameState {
 
 		//Scales buttons and logo according to width of screen
 		imageScale = viewWidth / 2 / sprites().get("play").getWidth();
-		logoScale = viewWidth / 1.5f / sprites().get("logo").getWidth();
+		logoScale = viewWidth / 1.2f / sprites().get("logo").getWidth();
 
 		//Sets max and min scale values based on the size, and original logoMaxScale
 		logoMinScale = logoScale * (1 - logoMaxScale);
@@ -137,12 +138,15 @@ public class MenuGameState implements GameState {
         
 
 		glUtil().setColor(ColorF.WHITE);
-		//TODO: Draw background
+
+		//Draws Background
+		Sprite2D bg = sprites().get("background");
+		bg.render(0, new V3F(0, 0, 0), Compass.NORTHWEST, viewWidth / bg.getWidth(), viewHeight / bg.getHeight(), 0);
 
 		//Draws play and quit buttons
 		sprites().get("play").render(0, playPos, Compass.CENTER, imageScale, imageScale, 0);
 		sprites().get("quit").render(0, quitPos, Compass.CENTER, imageScale, imageScale,0);
 		//Draws logo
-		sprites().get("logo").render(0, new V3F(viewWidth / 2, viewHeight / 4, 0), Compass.CENTER, logoScale , logoScale, logoRotation);
+		sprites().get("logo").render(0, new V3F(viewWidth / 2, viewHeight / 6, 0), Compass.CENTER, logoScale , logoScale, logoRotation);
     }
 }
