63be3769ffdd83fd899cd65eda4fd098594c3cb7
OleKN <ok.nakken@gmail.com>
2014-03-23 18:17:00 +0100

    Added tiling background to game

--- MonoBall/MonoBallLogic/src/com/devikaas/monoball/ingame/view/BallGameView.java
+++ MonoBall/MonoBallLogic/src/com/devikaas/monoball/ingame/view/BallGameView.java
@@ -3,6 +3,14 @@ package com.devikaas.monoball.ingame.view;
 import owg.engine.Engine;
 
 import com.devikaas.monoball.ingame.model.BallGameModel;
+import owg.engine.graphics.ColorF;
+import owg.engine.graphics.Sprite2D;
+import owg.engine.util.Calc;
+import owg.engine.util.Compass;
+import owg.engine.util.V3F;
+
+import static owg.engine.Engine.sprites;
+
 /**Specifies a view of the entire game model.*/
 public class BallGameView {
 	private CameraView camera;
@@ -14,7 +22,8 @@ public class BallGameView {
 	/**Creates a new game view attached to the indicated game model.*/
 	public BallGameView(BallGameModel model) {
 		this.model = model;
-		
+
+
 		camera = new CameraView(model.getCamera());
 		map = new MapView(this, model.getMap());
 		hud = new HUDView(model);
@@ -25,6 +34,23 @@ public class BallGameView {
 		long currentTime = System.currentTimeMillis();
 		float alpha = (currentTime-Engine.scene().getLastStepTime())/tickMs;
 		camera.render(alpha);
+
+
+		Sprite2D BG = sprites().get("backgroundTile");
+		float multiplication = (float)Math.floor(model.getCamera().getCurrentLocation().y() / model.getCamera().getHeight())/* + Calc.cyclic(model.getCamera().getCurrentLocation().y() * 0.8f / BG.getHeight(), 1f)*/;
+
+		BG.render(0,
+				new V3F(0, model.getCamera().getHeight() * multiplication, 0),
+				Compass.NORTHWEST,
+				model.getCamera().getWidth() / BG.getWidth(),
+				model.getCamera().getHeight() / BG.getHeight(),
+				0);
+		BG.render(0,
+				new V3F(0, model.getCamera().getHeight() * (multiplication + 1), 0),
+				Compass.NORTHWEST, model.getCamera().getWidth() / BG.getWidth(),
+				model.getCamera().getHeight() / BG.getHeight(),
+				0);
+
 		new SpriteView(model.getBall()).render(alpha);
 		map.render(alpha);
 		hud.render(alpha);
