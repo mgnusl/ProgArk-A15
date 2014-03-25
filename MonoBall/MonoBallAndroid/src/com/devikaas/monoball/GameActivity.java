c684831448d170a00a69a443f74e409ff0803603
bvx89 <bvx89.at.cs@gmail.com>
2014-03-25 12:02:19 +0100

    Added splash screen on startup

---
+++ MonoBall/MonoBallAndroid/src/com/devikaas/monoball/GameActivity.java
@@ -0,0 +1,32 @@
+package com.devikaas.monoball;
+
+import android.app.Activity;
+import android.os.Bundle;
+import owg.engine.Engine;
+import owg.engine.EntryPoint;
+import owg.engine.GameState;
+
+/**
+ * Created by bvx89 on 24/03/14.
+ */
+public class GameActivity extends Activity implements EntryPoint {
+
+    private GameActivity instance;
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+        instance = this;
+
+        Engine.initializeEngine(Engine.TargetPlatform.AndroidGLES1, instance);
+
+    }
+
+
+
+    @Override
+    public GameState getInitialState() {
+        return new MenuGameState();
+    }
+}
