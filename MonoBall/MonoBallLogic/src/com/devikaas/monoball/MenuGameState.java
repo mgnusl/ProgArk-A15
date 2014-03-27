577060ff1a4225d6cbc2acb452b7e1d98667ac55
bvx89 <bvx89.at.cs@gmail.com>
2014-03-27 04:05:51 +0100

    Performance optimizations to reduce load time

--- MonoBall/MonoBallLogic/src/com/devikaas/monoball/MenuGameState.java
+++ MonoBall/MonoBallLogic/src/com/devikaas/monoball/MenuGameState.java
@@ -24,18 +24,6 @@ public class MenuGameState implements GameState {
 	private float imageScale;
 	private float logoScale;
 
-
-	// Used to dynamically rotate logo.
-	private float logoRotation = 0.0f;
-	private float logoRotationSpeed = 0.0021f;
-	private float logoMaxAngle = 0.1f;
-
-	// Used to dynamically scale logo. logoMinScale will be set according to logoMaxScale.
-	// logoMaxScale will be changed to accomodate actual logoScale
-	private float logoMinScale;
-	private float logoMaxScale = 0.12f;
-	private float logoScaleSpeed = 0.0011f;
-
 	//Stores position and size of buttons, to allow easy interaction
 	private V3F playPos;
 	private V3F quitPos;
@@ -62,6 +50,7 @@ public class MenuGameState implements GameState {
 		// subImage = 0;
 		// font = new SpriteFontRenderer(sprites().get("font"), 1, 1);
 		viewSpacePointer = new V3F();
+
 		// audioLib().get("clank").play(1, 0, 1);
 
         sysController = new SystemKeyController();
@@ -71,29 +60,24 @@ public class MenuGameState implements GameState {
 		viewHeight = 320;
 		viewWidth = viewHeight*screenAspect;
 
-		//Scales buttons and logo according to width of screen
-		imageScale = viewWidth / 2 / sprites().get("play").getWidth();
-		logoScale = viewWidth / 1.2f / sprites().get("logo").getWidth();
-
-		//Sets max and min scale values based on the size, and original logoMaxScale
-		logoMinScale = logoScale * (1 - logoMaxScale);
-		logoMaxScale = logoScale * (1 + logoMaxScale);
-
-		//Sets button positions based on screen size, and scales according to screen width.
-		playPos = new V3F(viewWidth/2, viewHeight/2, 0);
-		playSize = new V3F(	sprites().get("play").getWidth() * imageScale,
-				sprites().get("play").getHeight() * imageScale, 0);
-
-		//Sets position of quit button below play button
-		quitPos = new V3F(playPos.x(), playPos.y() + playSize.y()*1.1f, 0);
-		quitSize = playSize;
-
-
         // Get references to sprites
         backgroundTile = sprites().get("background");
         logo = sprites().get("logo");
         quit = sprites().get("quit");
         play = sprites().get("play");
+
+		//Scales buttons and logo according to width of screen
+		imageScale = viewWidth / 2 / play.getWidth();
+		logoScale = viewWidth / 1.2f / logo.getWidth();
+
+		//Sets button positions based on screen size, and scales according to screen width.
+		playPos = new V3F(viewWidth/2, viewHeight/2, 0);
+		playSize = new V3F(	play.getWidth() * imageScale,
+				            play.getHeight() * imageScale, 0);
+
+		//Sets position of quit button below play button
+		quitPos = new V3F(playPos.x(), playPos.y() + playSize.y()*1.1f, 0);
+		quitSize = playSize;
 
         bgReplicates = (int)Math.ceil(viewHeight / (viewWidth / backgroundTile.getWidth() * backgroundTile.getHeight()));
         System.out.println("Bg replicates:" + bgReplicates);
@@ -101,19 +85,6 @@ public class MenuGameState implements GameState {
 	}
     @Override
     public void step() {
-
-		// Turns rotation if outside of max or min values, and rotates logo
-		if(logoRotation > logoMaxAngle || logoRotation < - logoMaxAngle)
-			logoRotationSpeed = -logoRotationSpeed;
-
-		logoRotation += logoRotationSpeed;
-
-		// Turns scaling if outside of max or min values, and rescales the logo
-		if(logoScale > logoMaxScale || logoScale < logoMinScale)
-			logoScaleSpeed = - logoScaleSpeed;
-
-		logoScale += logoScaleSpeed;
-
 
     	//Example: fixed camera height, stretched to fit the screen with the correct aspect ratio
 
@@ -136,7 +107,11 @@ public class MenuGameState implements GameState {
 					viewSpacePointer.y() < quitPos.y() + quitSize.y() / 2){
 				Engine.exit(0);
 			}
-		}
+
+            System.out.println("click!");
+            System.out.println("X: " + viewSpacePointer.x());
+            System.out.println("Y: " + viewSpacePointer.y());
+        }
 
         sysController.step();
     }
@@ -175,6 +150,6 @@ public class MenuGameState implements GameState {
 		play.render(0, playPos, Compass.CENTER, imageScale, imageScale, 0);
 		quit.render(0, quitPos, Compass.CENTER, imageScale, imageScale,0);
 		//Draws logo
-		logo.render(0, new V3F(viewWidth / 2, viewHeight / 6, 0), Compass.CENTER, logoScale , logoScale, logoRotation);
+		logo.render(0, new V3F(viewWidth / 2, viewHeight / 6, 0), Compass.CENTER, logoScale , logoScale, 0);
     }
 }
